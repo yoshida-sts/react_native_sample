c69f830c7f8eda6bca71beb6023256af9d465705
開発 <sts_yoshida@ymail.ne.jp>
2023-05-16 08:50:27 +0900

    deleted prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,31 @@
-export default {
-  "env": {
-    "browser": true,
-    "es2021": true,
+module.exports = {
+  env: {
+    node: true,
+    // "browser": true,
+    // "es2021": true,
   },
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
   ],
-  "overrides": [],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  "plugins": [
-    "react",
-    "react-hooks",
-    "@typescript-eslint",
-    "prettier"
-  ],
-  "rules": {
-    indent: ["error", 2, { SwitchCase: 1 }],
-    quotes: ["error", "double", { avoidEscape: true }],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
+  plugins: [ "react", "react-hooks", "@typescript-eslint" ],
+  rules: {
+    indent: [ "error", 2, { SwitchCase: 1 } ],
+    quotes: [ "error", "double", { avoidEscape: true } ],
+    "array-bracket-spacing": [ "error", "always" ],
+    "object-curly-spacing": [ "error", "always" ],
+    "linebreak-style": [ "error", "unix" ],
+    semi: [ "error", "always" ],
     "no-empty-function'": "off",
     "@typescript-eslint/no-empty-function": "off",
     "react/display-name2": "off",
     "react/prop-types": "off",
-    "prettier/prettier": "error",
   },
 };
