646892be0ff6b31ea6fbf1844803f69d3dd963c2
開発 <sts_yoshida@ymail.ne.jp>
2023-05-20 10:12:53 +0900

    implemented func to display movie item

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
+import axios from "axios";
+
+type movie = {
+  original_title?: string;
+  original_language?: string;
+}
+
+const url = "https://api.themoviedb.org/3/movie/popular?api_key=yourapikey";
+
+const getpopularMovies = async () => {
+  const response = await axios.get(url);
+  return response.data.results;
+};
 
 const App: React.FC = () => {
+  const [ movie, setMovie ] = useState<movie>({}); 
+  getpopularMovies().then(movies => {
+    setMovie(movies[0]);
+  });
   return(
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text style={{ textAlign: "center" }}>
-        Try editing me!
-      </Text>
+    <View style={{ 
+      flex: 1, 
+      justifyContent: "center", 
+      alignItems: "center" 
+    }}>
+      <Text>Movie Title: { movie.original_title }</Text>
+      <Text>Language: { movie.original_language }</Text>
     </View>
   );
 };
